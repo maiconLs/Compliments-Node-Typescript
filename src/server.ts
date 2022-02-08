1483cd1980a61dcadb37b2912e19a6317a658028
Maicon <maiconlsilva10@gmail.com>
2022-02-07 22:07:43 -0300

    Documentando com Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,36 @@
-import 'reflect-metadata';
-import express, { Request, Response, NextFunction} from "express";
-import 'express-async-errors';
-import cors from 'cors';
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
-import { router } from './routes'
+import { router } from "./routes";
 
-import './database'
+import "./database";
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof Error){
-    return response.status(400).json({
-      Error: err.message,
-    })
-}
-  return response.status(500).json({
-    status: "error",
-    message: "Internal Server Error",
-  })
-}
-)
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        Error: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
 
 app.listen(3000, () => console.log("Server is running"));
